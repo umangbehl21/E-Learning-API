e682486bbbe9ceb0d782905364288540aa63c19d
Umang Behl <98390701+umangbehl21@users.noreply.github.com>
2024-04-25 13:55:27 +0530

    updated

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -10,16 +10,12 @@ const knexInstance = knex(knexConfig.development); // Use knexConfig.development
 const uploadProfilePicture = async (req, res) => {    try {
 
   const profilePicture = req.file.fieldname;
-  console.log(req.file)
+
       if (!req.file || !profilePicture) {
         return res.status(400).json({ error: 'No file uploaded' });
       }
- 
-      //console.log(req.file)  
-
       // Upload image to Cloudinary
       const result = await cloudinary.uploader.upload(req.file.path);
-      console.log(result)
       // Get user ID from authentication (pseudo-code)
       const userId = req.user.userId;
   
@@ -34,19 +30,20 @@ const uploadProfilePicture = async (req, res) => {    try {
       res.status(500).json({ error: 'Failed to upload profile picture' });
     }
   };
+
   
   // Controller function for updating a profile picture
   const updateProfilePicture = async (req, res) => {
     try {
-      const profilePicture = req.file.profilePicture;
-      if (!req.files || !req.files.profilePicture) {
+      const profilePicture = req.file.fieldname;
+      if (!req.file || !profilePicture) {
         return res.status(400).json({ error: 'No file uploaded' });
       }
   
       // Upload new image to Cloudinary
-      const result = await cloudinary.uploader.upload(profilePicture.tempFilePath);
+      const result = await cloudinary.uploader.upload(req.file.path);
     // Get user ID from authentication (pseudo-code)
-      const userId = req.user.id;
+      const userId = req.user.userId;
 
     // Update user's profile picture in the database
       await knexInstance('users').where('id', userId).update({ profile_picture: result.public_id });
